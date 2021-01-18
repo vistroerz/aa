2be7d200dcde0c290f79c6d1e23c824c1224120d
vistroerz <75893364+vistroerz@users.noreply.github.com>
2021-01-18 10:35:13 +0300

    pyhthon-task-1

---
+++ task-1.py
@@ -0,0 +1,9 @@
+print("hello")
+my_name = "Fatih"
+a = input("Input your name as first character uppercase =")
+
+if a == my_name :
+    print("Hello, Fatih! The password is : W@12")
+else :
+    print("Hello, {}! See you later.".format(a))
+
